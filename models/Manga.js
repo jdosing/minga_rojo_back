0eca62a68a97cac8675bbec9b9f0ab605ebfe2b3
jdosing <96783149+jdosing@users.noreply.github.com>
2023-04-21 16:52:44 -0600

    tarea6-7 (#10)

---
+++ models/Manga.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+let schema = new mongoose.Schema ({
+    autor_id: {
+        type: mongoose.Types.ObjectId,
+        ref: "authors"
+    },
+    company_id: {
+        type: mongoose.Types.ObjectId,
+        ref: "companies"
+    },
+    title: {type:String, required:true},
+    cover_photo: { type: String,required:true},
+    description: {type:String, required: true},
+    category_id: {
+        type: mongoose.Types.ObjectId,
+        ref:"categories",
+        required: true
+    }
+
+},{
+    timestamps: true
+})
+let collection = "mangas"
+
+let Manga = mongoose.model(collection,schema)
+export default Manga;
