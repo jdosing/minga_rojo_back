c999fd2db97b0a59dc087f1de971d6637b8acec1
jdosing <96783149+jdosing@users.noreply.github.com>
2023-04-24 13:25:01 -0500

    cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import express from 'express'
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import cors from `cors`
+import cors from 'cors'
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
 import {__dirname} from './utils.js'
@@ -19,6 +19,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
